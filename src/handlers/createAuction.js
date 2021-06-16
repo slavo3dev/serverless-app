1e187a7ce8031bc64ce5d40d36bc693b4e039303
slavo3dev <slavo@slavo3.com>
2021-06-16 17:13:48 -0400

    fixing code template

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -1,14 +1,26 @@
+import {v4 as uuid} from 'uuid';
+import AWS from 'aws-sdk';
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
 async function createAuction(event,context) {
   const {title} = JSON.parse(event.body);
   const now = new Date();
   const auction = {
+    id: uuid(),
     title,
     status: 'OPEN',
     createdAt: now.toISOString()
   };
+
+  await dynamoDb.put({
+    TableName: 'AuctionTable',
+    Item: auction
+  }).promise();
+
   return {
     statusCode: 201,
-    body: JSON.stringify(auction),
+    body: JSON.stringify(auction)
   };
 };
 
