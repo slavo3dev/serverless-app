a3998ae4aff5972ad30ca4d10bd0087ff66af176
slavo3dev <slavo@slavo3.com>
2021-06-30 17:09:47 -0400

    cleaning code

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -1,9 +1,6 @@
 import {v4 as uuid} from 'uuid';
 import AWS from 'aws-sdk';
-import middy from "@middy/core"
-import httpJsonBodyParser from "@middy/http-json-body-parser"
-import httpEventNormalizer from '@middy/http-event-normalizer';
-import httpErrorHandler from '@middy/http-error-handler';
+import handlerMiddy from "../lib/util"
 import createError from "http-errors"
 
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
@@ -34,9 +31,7 @@ async function createAuction(event,context) {
   };
 };
 
-export const handler = middy(createAuction)
-  .use(httpJsonBodyParser())
-  .use(httpEventNormalizer())
-  .use(httpErrorHandler())
+export const handler = handlerMiddy(createAuction)
+ 
 
 
--- src/handlers/getAuctions.js
+++ src/handlers/getAuctions.js
@@ -1,8 +1,5 @@
 import AWS from 'aws-sdk';
-import middy from "@middy/core"
-import httpJsonBodyParser from "@middy/http-json-body-parser"
-import httpEventNormalizer from '@middy/http-event-normalizer';
-import httpErrorHandler from '@middy/http-error-handler';
+import handlerMiddy from "../lib/util"
 import createError from "http-errors"
 
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
@@ -28,7 +25,5 @@ async function getAuctions(event,context) {
   };
 };
 
-export const handler = middy(getAuctions)
-  .use(httpJsonBodyParser())
-  .use(httpEventNormalizer())
-  .use(httpErrorHandler())
+export const handler = handlerMiddy(getAuctions)
+ 
