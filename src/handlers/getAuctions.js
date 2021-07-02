8f5f48ed8744d6ea9aa114145f9a48412761c075
slavo3dev <slavo@slavo3.com>
2021-07-02 11:04:54 -0400

    status fucntion

--- src/handlers/getAuctions.js
+++ src/handlers/getAuctions.js
@@ -6,11 +6,22 @@ const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
 async function getAuctions(event,context) {
   let auctions;
+  const {status} = event.queryStringParameters;
+  
+  const params = {
+    TableName: process.env.AUCTIONS_TABLE_NAME,
+    IndexName: 'statusAndEndDate',
+    KeyConditionExpression: '#status = :status',
+     ExpressionAttributeValues: {
+      ':status': status,
+    },
+    ExpressionAttributeNames: {
+      '#status': 'status',
+    },
+  }
   
   try {
-    const result = await dynamoDb.scan({
-      TableName: process.env.AUCTIONS_TABLE_NAME,
-    }).promise()
+    const result = await dynamoDb.query(params).promise()
 
     auctions = result.Items
   }
