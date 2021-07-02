9b65701358206ec42d30c6b572921fb329af3a2d
slavo3dev <slavo@slavo3.com>
2021-07-02 10:33:12 -0400

    close auctions

--- src/handlers/processAuctions.js
+++ src/handlers/processAuctions.js
@@ -1,11 +1,10 @@
-import { getEnedAuctions } from "../lib/util"
+import { getEnedAuctions, closeAuction } from "../lib/util"
 
 async function processAuctions(event,context) {
     
     const auctionsToClose = await getEnedAuctions();
-    console.log("Auctions to Close", auctionsToClose)
-
-    
+    const closePromises = auctionsToClose.map(auction => closeAuction(auction.id))
+    await Promise.all(closePromises)  
 }
 
 export const handler = processAuctions
--- src/lib/util.js
+++ src/lib/util.js
@@ -29,3 +29,20 @@ export const getEnedAuctions = async () => {
   return result.Items;
    
 }
+
+export const closeAuction = (auction) => {
+  const params = {
+    TableName: process.env.AUCTIONS_TABLE_NAME,
+    Key: { id: auction.id },
+    UpdateExpression: 'set #status = :status',
+    ExpressionAttributeValues: {
+      ':status': 'CLOSED',
+    },
+    ExpressionAttributeNames: {
+      '#status': 'status',
+    },
+  };
+
+  const result = await dynamodb.update(params).promise();
+  return result;
+}
