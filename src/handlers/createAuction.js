a8165787ab6f35c62ce2189a5caa5ae4ee00f579
slavo3dev <slavo@slavo3.com>
2021-07-07 13:54:27 -0400

    adding bidder

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -9,6 +9,7 @@ const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
 async function createAuction(event,context) {
   const {title} = event.body;
+  const {email} = event.requestContext.authorizer;
   const now = new Date();
   const endDate = new Date();
   endDate.setHours(now.getHours() + 1)
@@ -22,7 +23,8 @@ async function createAuction(event,context) {
     highestBid: {
       amount: 0,
       
-    }
+    },
+    seller: email
   };
 
   try {
